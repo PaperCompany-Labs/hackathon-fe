6d11b44c4e0949c606c6bdabe61101a606da6f82
gayeon7877 <gayean01@gmail.com>
2025-02-10 15:10:17 +0900

    fix: api, style

--- login.js
+++ login.js
@@ -13,19 +13,21 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
             },
             body: JSON.stringify({ id: userId, password: password })
         });
-        //
+
         const data = await response.json();
         console.log("API 응답 상태 코드:", response.status);
         console.log("응답 원본 데이터:", data);
 
         if (response.status === 200 && data.access_token) {
-            localStorage.setItem('access_token', data.access_token); // 토큰 저장
+            // 토큰과 유저 번호를 localStorage에 저장 (다른 페이지에서도 사용 가능)
+            localStorage.setItem('access_token', data.access_token);
+            localStorage.setItem('user_no', data.user_no);
             console.log("로그인 후 저장된 토큰:", localStorage.getItem('access_token'));
+            console.log("로그인 후 저장된 사용자 번호:", localStorage.getItem('user_no'));
 
-            //alert('로그인 성공!');
             window.location.href = "shorts.html"; // 로그인 후 이동
         } else {
-            alert('로그인 실패! 아이디 또는 비밀번호를 확인하세요.');
+            //alert('로그인 실패! 아이디 또는 비밀번호를 확인하세요.');
         }
     } catch (error) {
         console.error('로그인 요청 실패:', error);
